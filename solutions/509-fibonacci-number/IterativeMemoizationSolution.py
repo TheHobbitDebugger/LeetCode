5753a5788e4e3fb3366e1c1617caa2b0a3751087
Bob <adriano.mastergoods@gmail.com>
2026-06-23 11:35:08 +0200

    509-fibonacci golden_ratio solution, iterativeMemoization solution

---
+++ solutions/509-fibonacci-number/IterativeMemoizationSolution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        prev = 0
+        cur = 0
+
+        for i in range(2, n+1):
+            prev,cur = cur, prev+cur
+
+        return cur
+    
+    #TIme: O(n)
+    #SPace: O(1)
